fcdc4b25ade67ff3ea553f7facc5932b215a04c8
Blanca Hernandez <blamca1029@gmail.com>
2018-09-12 14:06:33 -0700

    converted to react-redux

--- src/components/header.js
+++ src/components/header.js
@@ -5,13 +5,10 @@ import TopNav from './top-nav';
 import './header.css';
 
 export default function Header(props) {
-  return (
-    <header>
-      <TopNav
-        onGenerateAuralUpdate={() => props.onGenerateAuralUpdate()}
-        onRestartGame={() => props.onRestartGame()}
-      />
-      <h1>HOT or COLD</h1>
-    </header>
-  );
+    return (
+        <header>
+        <TopNav />
+        <h1>HOT or COLD</h1>
+        </header>
+    );
 }
